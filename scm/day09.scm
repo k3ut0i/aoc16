fd255b438b42e86c547e50717fcf828f3dc79366
keutoi <k3tu0isui@gmail.com>
2021-07-28 21:56:47 +0530

    day09: part2 in scheme

--- scm/day09.scm
+++ scm/day09.scm
@@ -13,22 +13,48 @@
   (cons (string->number (match:substring m 1))
 	(string->number (match:substring m 2))))
 
-(define (iterate-and-count str)
+(define (iterate-and-count/gen str handler)
   (let loop ((i 0)
 	     (count (string-length str)))
-
     (let ((m (regexp-exec marker-regexp str i)))
       (if m
-	  (let* ((marker-len (string-length (match:substring m 0)))
-		 (marker (match->marker m))
-		 (len (car marker))
-		 (mult (cdr marker)))
-	    (loop (+ (match:end m) len)
-		  (+ count (* (- mult 1) len) (- marker-len))))
+	  (let* ((handler-result (handler m str))
+		 (skip-to (car handler-result))
+		 (new-count (+ count (cdr handler-result))))
+	    (loop skip-to new-count))
 	  count))))
+
+(define (iterate-and-count/inert str)
+  (iterate-and-count/gen
+   str
+   (lambda (m s)
+     (let* ((marker-len (string-length (match:substring m 0)))
+	    (marker (match->marker m))
+	    (len (car marker))
+	    (mult (cdr marker)))
+       (cons (+ (match:end m) len)
+	     (+ (* (- mult 1) len) (- marker-len)))))))
+
+(define (iterate-and-count/explosive str)
+  (iterate-and-count/gen
+   str
+   (lambda (m s)
+     (let* ((marker-len (string-length (match:substring m 0)))
+ 	    (marker (match->marker m))
+	    (len (car marker))
+	    (mult (cdr marker))
+	    (next-str (substring s (match:end m)
+				 (+ (match:end m) len))))
+       (cons (+ (match:end m) len)
+	     (+ (* mult (iterate-and-count/explosive next-str))
+		(- (+ marker-len len))))))))
+
+;; I could have combined both inert and explosive variants but
+;; it would be too hairy
 
 (define (main file)
   (let* ((port (open-file file "r"))
 	 (s (read-delimited "\n" port)))
     (close port)
-    (iterate-and-count s)))
+    (values (iterate-and-count/inert s)
+	    (iterate-and-count/explosive s))))
